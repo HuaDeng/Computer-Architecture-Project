f791467381beb8524e364768380168e273f48551
Coda Phillips <codaphillips@gmail.com>
2015-03-28 13:02:42 -0500

    Change parameters to macros in wiscsc15_ctrl

--- wiscsc15_ctrl.v
+++ wiscsc15_ctrl.v
@@ -1,37 +1,59 @@
 /*CS 552 Project WISC-SC15 control unit*/
+
+//instruction: add, sub, nand, xor
+`define ARITH_INSTR 4'b00??
+
+//instruction: inc
+`define INC_INSTR 4'B0100
+
+ //instruction: sra
+`define SRA_INSTR 4'B0101
+
+//instruction: srl and sll
+`define SL_INSTR 4'B011?
+
+//instruction: lw
+`define LW_INSTR 4'B1000
+
+//instruction: sw
+`define SW_INSTR 4'B1001
+
+//instruction: lhb
+`define LHB_INSTR 4'B1010
+
+//instruction: llb
+`define LLB_INSTR 4'B1011
+
+//instruction: b
+`define BRCH_INSTR 4'b1100
+
+//instruction:call
+`define CALL_INSTR 4'b1101
+
+//instruction; return
+`define RET_INSTR 4'B1110
+
 module wiscsc15_ctrl(Opcode, pc_src, rf_wsrc, rf_rsrc1, rf_rsrc2, rf_w, alu_src1, alu_src2, sel_call, sel_branch, aluop, dm_in, dm_addr, dm_read, dm_write, rf_data);
-	
-	input [3:0] Opcode;
-	
-	output reg pc_src;
-	output reg rf_wsrc;
-	output reg [1:0] rf_rsrc1;
-	output reg [1:0] rf_rsrc2;
-	output reg rf_w;
-	output reg alu_src1;
-	output reg [1:0] alu_src2;
-	output reg sel_call;
-	output reg sel_branch;
-	output reg [2:0] aluop;
-	output reg dm_in;
-	output reg dm_addr;
-	output reg dm_read;
-	output reg dm_write;
-	output reg [1:0] rf_data;
 
-	parameter arith_instr = 4'b00??; //instruction: add, sub, nand, xor
-	parameter inc_instr = 4'b0100; //instruction: inc
-	parameter sra_instr = 4'b0101; //instruction: sra
-	parameter sl_instr = 4'b011?; //instruction: srl and sll
-	parameter lw_instr = 4'b1000; //instruction: lw
-	parameter sw_instr = 4'b1001; //instruction: sw 
-	parameter lhb_instr = 4'b1010; //instruction: lhb
-	parameter llb_instr = 4'b1011; //instruction: llb
-	parameter brch_instr = 4'b1100; //instruction: b
-	parameter call_instr = 4'b1101; //instruction:call
-	parameter ret_instr = 4'b1110; //instruction; return
+    input [3:0] Opcode;
 
-	always @(Opcode) begin
+    output reg pc_src;
+    output reg rf_wsrc;
+    output reg [1:0] rf_rsrc1;
+    output reg [1:0] rf_rsrc2;
+    output reg rf_w;
+    output reg alu_src1;
+    output reg [1:0] alu_src2;
+    output reg sel_call;
+    output reg sel_branch;
+    output reg [2:0] aluop;
+    output reg dm_in;
+    output reg dm_addr;
+    output reg dm_read;
+    output reg dm_write;
+    output reg [1:0] rf_data;
+
+    always @(Opcode) begin
 
         // Common outputs
         pc_src = 1'b0;
@@ -50,23 +72,23 @@ module wiscsc15_ctrl(Opcode, pc_src, rf_wsrc, rf_rsrc1, rf_rsrc2, rf_w, alu_src1
         dm_write = 1'b0;
         rf_data = 2'b11;
 
-		casez (Opcode)
-            arith_instr: begin
+        casez (Opcode)
+            `ARITH_INSTR: begin
             end
 
-            inc_instr:   begin
+            `INC_INSTR:   begin
                 alu_src2 = 2'b10;
             end
 
-            sra_instr: 	 begin
+            `SRA_INSTR:   begin
                 alu_src2 = 2'b01;
             end
 
-            sl_instr:    begin
+            `SL_INSTR:    begin
                 alu_src2 = 2'b01;
             end
 
-            lw_instr:    begin
+            `LW_INSTR:    begin
                 rf_rsrc2 = 2'b01;
                 alu_src1 = 1'b1;
                 alu_src2 = 2'b11;
@@ -75,7 +97,7 @@ module wiscsc15_ctrl(Opcode, pc_src, rf_wsrc, rf_rsrc1, rf_rsrc2, rf_w, alu_src1
                 rf_data = 2'b00;
             end
 
-            sw_instr:    begin
+            `SW_INSTR:    begin
                 rf_wsrc = 1'bx;
                 rf_rsrc1 = 2'b01;
                 rf_rsrc2 = 2'b01;
@@ -88,7 +110,7 @@ module wiscsc15_ctrl(Opcode, pc_src, rf_wsrc, rf_rsrc1, rf_rsrc2, rf_w, alu_src1
                 rf_data = 2'bxx;
             end
 
-            lhb_instr:   begin
+            `LHB_INSTR:   begin
                 rf_rsrc1 = 2'b01;
                 rf_rsrc2 = 2'bxx;
                 alu_src1 = 1'bx;
@@ -96,7 +118,7 @@ module wiscsc15_ctrl(Opcode, pc_src, rf_wsrc, rf_rsrc1, rf_rsrc2, rf_w, alu_src1
                 rf_data = 2'b01;
             end
 
-            llb_instr:	 begin
+            `LLB_INSTR:   begin
                 rf_rsrc1 = 2'b01;
                 rf_rsrc2 = 2'bxx;
                 alu_src1 = 1'bx;
@@ -104,7 +126,7 @@ module wiscsc15_ctrl(Opcode, pc_src, rf_wsrc, rf_rsrc1, rf_rsrc2, rf_w, alu_src1
                 rf_data = 2'b10;
             end
 
-            brch_instr:  begin
+            `BRCH_INSTR:  begin
                 rf_w = 1'b0;
                 sel_branch = 1'b1;
                 aluop = 3'b000;
@@ -113,7 +135,7 @@ module wiscsc15_ctrl(Opcode, pc_src, rf_wsrc, rf_rsrc1, rf_rsrc2, rf_w, alu_src1
                 rf_data = 2'b00;
             end
 
-            call_instr:  begin
+            `CALL_INSTR:  begin
                 rf_wsrc = 1'b0;
                 rf_rsrc1 = 2'b10;
                 rf_rsrc2 = 2'b10;
@@ -124,7 +146,7 @@ module wiscsc15_ctrl(Opcode, pc_src, rf_wsrc, rf_rsrc1, rf_rsrc2, rf_w, alu_src1
                 dm_write = 1'b1;
             end
 
-            ret_instr:   begin
+            `RET_INSTR:   begin
                 pc_src = 1'b1;
                 rf_wsrc = 1'b0;
                 rf_rsrc1 = 2'b10;
@@ -150,6 +172,6 @@ module wiscsc15_ctrl(Opcode, pc_src, rf_wsrc, rf_rsrc1, rf_rsrc2, rf_w, alu_src1
                 dm_write = 1'bx;
                 rf_data = 2'bxx;
             end
-		endcase							   
-	end 
+        endcase
+    end
 endmodule 
