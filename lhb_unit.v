cea3919d0e0d55cd67ce86b6f7258f89fd7d5a3e
Coda Phillips <codaphillips@gmail.com>
2015-03-29 09:36:57 -0500

    Use continuous assignment in lhb_unit and llb_unit, otherwise it doesn't synthesize

--- lhb_unit.v
+++ lhb_unit.v
@@ -3,9 +3,7 @@ module lhb_unit(in_1, in_2, out);
 	input [15:0] in_1;
 	input [7:0] in_2;
 
-	output reg [15:0] out;
+	output wire [15:0] out;
 
-	always @ (in_1 or in_2) begin
-		assign out = {in_2, in_1[7:0]};
-	end 
+    assign out = {in_2, in_1[7:0]};
 endmodule 
--- llb_unit.v
+++ llb_unit.v
@@ -3,9 +3,7 @@ module llb_unit(in_1, in_2, out);
 	input [15:0] in_1;
 	input [7:0] in_2;
 
-	output reg [15:0] out;
+	output wire [15:0] out;
 
-	always @ (in_1 or in_2) begin
-		assign out = {in_1[15:8], in_2};
-	end 
+    assign out = {in_1[15:8], in_2};
 endmodule 
