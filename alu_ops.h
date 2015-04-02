dd14785012908a0c5cde6b926b2f987134d45cb8
Coda Phillips <codaphillips@gmail.com>
2015-04-02 01:25:31 -0500

    Factor alu_op defines into header

---
+++ alu_ops.h
@@ -0,0 +1,11 @@
+`ifndef ALU_H
+`define ALU_H
+`define ALU_ADD 3'b000
+`define ALU_SUB 3'b001
+`define ALU_NAND 3'b010
+`define ALU_XOR 3'b011
+`define ALU_INC 3'b100
+`define ALU_SRA 3'b101
+`define ALU_SRL 3'b110
+`define ALU_SLL 3'b111
+`endif
